0cd3893e05557644b5f16fd82bf4d663e5375691
Sean <sean.leitch@live.com>
2021-03-12 12:30:02 +0100

    pswd

--- project/NiceStartSL/app/src/main/java/com/seanleitch/nicestartsl/LoginActivity.java
+++ project/NiceStartSL/app/src/main/java/com/seanleitch/nicestartsl/LoginActivity.java
@@ -5,18 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText emailLogin;
+    EditText passwordLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        emailLogin = findViewById(R.id.emailLogin);
+        passwordLogin = findViewById(R.id.passwordLogin);
     }
 
     public void login(View view) {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+
+        String sEmail = emailLogin.getText().toString().trim();
+        String sPass = passwordLogin.getText().toString().trim();
+
+        if (sEmail.isEmpty() || sPass.isEmpty()) {
+            Toast.makeText(this, "Debes rellenar todos los campos", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
